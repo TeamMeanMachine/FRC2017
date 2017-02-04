81a09841e16a74beac9ebc628bfd0617f9622f8c
Justine P <justinetulip@gmail.com>
2017-02-04 15:42:38 -0800

    Made Fuel Intake not be gone

--- robot/src/main/java/org/team2471/frc/steamworks/HardwareMap.java
+++ robot/src/main/java/org/team2471/frc/steamworks/HardwareMap.java
@@ -2,6 +2,8 @@ package org.team2471.frc.steamworks;
 
 import com.ctre.CANTalon;
 
+import edu.wpi.first.wpilibj.Solenoid;
+
 public class HardwareMap {
   public static final class Drive {
     public static final CANTalon rightMotor1 = new CANTalon(0);
@@ -10,5 +12,13 @@ public class HardwareMap {
     public static final CANTalon leftMotor1 = new CANTalon(0);
     public static final CANTalon leftMotor2 = new CANTalon(0);
     public static final CANTalon leftMotor3 = new CANTalon(0);
+  }
+
+  public static final class FuelIntakeMap {
+    public static final CANTalon intakeMotor = new CANTalon(0);
+    public static final CANTalon leftWindshieldMotor = new CANTalon(0);
+    public static final CANTalon rightWindshieldMotor = new CANTalon(0);
+    public static final Solenoid intakeSolenoid = new Solenoid(0);
+
   }
 }
