13c1a5dc00610a5b2655736364656054cc8af8cb
Justine P <justinetulip@gmail.com>
2017-02-06 19:28:59 -0800

    Shooter Class

---
+++ robot/src/main/java/org/team2471/frc/steamworks/subsystems/Shooter.java
@@ -0,0 +1,13 @@
+package org.team2471.frc.steamworks.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Shooter extends Subsystem {
+
+
+
+  @Override
+  protected void initDefaultCommand() {
+
+  }
+}
