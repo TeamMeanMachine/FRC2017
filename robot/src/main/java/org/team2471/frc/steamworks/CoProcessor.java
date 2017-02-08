bac781230855cf670fc8d3ea1f9acbf78d8fa077
tylerscheuble <tylerscheuble@gmail.com>
2017-02-07 20:32:21 -0800

    Add coprocessor skeleton

---
+++ robot/src/main/java/org/team2471/frc/steamworks/CoProcessor.java
@@ -0,0 +1,17 @@
+package org.team2471.frc.steamworks;
+
+public class CoProcessor {
+  /**
+   * @return whether the camera has detected a shooter target
+   */
+  public boolean shooterTargetDetected() {
+    return false;
+  }
+
+  /**
+   * @return the offset of the shooter target in degrees
+   */
+  public double getShooterTargetError() {
+    return 0.0;
+  }
+}
