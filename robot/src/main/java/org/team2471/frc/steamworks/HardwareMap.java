b59d126fa8756d51114c3e3881583ba9b8ce89e4
Klingcm <klingcm@gmail.com>
2017-02-12 15:29:38 -0800

    CIM motors and Fuel Intake changed CANTalons

--- robot/src/main/java/org/team2471/frc/steamworks/HardwareMap.java
+++ robot/src/main/java/org/team2471/frc/steamworks/HardwareMap.java
@@ -6,18 +6,18 @@ import edu.wpi.first.wpilibj.Solenoid;
 
 public class HardwareMap {
   public static final class Drive {
-    public static final CANTalon rightMotor1 = new CANTalon(0);
-    public static final CANTalon rightMotor2 = new CANTalon(0);
-    public static final CANTalon rightMotor3 = new CANTalon(0);
+    public static final CANTalon rightMotor1 = new CANTalon(15);
+    public static final CANTalon rightMotor2 = new CANTalon(14);
+    public static final CANTalon rightMotor3 = new CANTalon(13);
     public static final CANTalon leftMotor1 = new CANTalon(0);
-    public static final CANTalon leftMotor2 = new CANTalon(0);
-    public static final CANTalon leftMotor3 = new CANTalon(0);
+    public static final CANTalon leftMotor2 = new CANTalon(1);
+    public static final CANTalon leftMotor3 = new CANTalon(2);
   }
 
   public static final class FuelIntakeMap {
-    public static final CANTalon intakeMotor = new CANTalon(0);
-    public static final CANTalon leftWindshieldMotor = new CANTalon(0);
-    public static final CANTalon rightWindshieldMotor = new CANTalon(0);
+    public static final CANTalon intakeMotor = new CANTalon(3);
+    public static final CANTalon leftWindshieldMotor = new CANTalon(9);
+    public static final CANTalon rightWindshieldMotor = new CANTalon(8);
     public static final Solenoid intakeSolenoid = new Solenoid(0);
 
   }
