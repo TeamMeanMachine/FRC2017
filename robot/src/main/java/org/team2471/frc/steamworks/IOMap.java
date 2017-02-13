46765d53b9e45af29b7fc5121088b5cd9982d820
Klingcm <klingcm@gmail.com>
2017-02-12 17:38:17 -0800

    deleted gear spit, fixed intake, IOMap edited

--- robot/src/main/java/org/team2471/frc/steamworks/IOMap.java
+++ robot/src/main/java/org/team2471/frc/steamworks/IOMap.java
@@ -5,6 +5,7 @@ import org.team2471.frc.lib.io.Controller;
 import org.team2471.frc.lib.io.ControllerAxis;
 import org.team2471.frc.lib.io.ControllerButton;
 import org.team2471.frc.steamworks.commands.FuelIntakeCommand;
+import org.team2471.frc.steamworks.commands.IntakeGearCommand;
 
 public class IOMap {
   private static Controller driverController = new Controller(0);
@@ -27,9 +28,13 @@ public class IOMap {
 
   public static final ControllerButton turnInPlaceButton = driverController.getButton(0);
 
+  public static final ControllerButton gearIntakeButton = coDriverController.getButton(7);
+
 
   public static void init () {
     CommandTrigger intakeTrigger = new CommandTrigger(intakeButton::get);
     intakeTrigger.whileActive(new FuelIntakeCommand());
+    CommandTrigger gearIntake = new CommandTrigger(gearIntakeButton::get);
+    intakeTrigger.whileActive(new IntakeGearCommand());
   }
 }
