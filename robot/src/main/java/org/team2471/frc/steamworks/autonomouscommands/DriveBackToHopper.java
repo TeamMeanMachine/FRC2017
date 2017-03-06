159a67999d29cd9db501b2dba9967c29d2aea23f
James-Kling <klingjt@gmail.com>
2017-03-05 19:05:17 -0800

    made the HunderedPointAuto hit the hopper from the back

--- robot/src/main/java/org/team2471/frc/steamworks/autonomouscommands/DriveBackToHopper.java
+++ robot/src/main/java/org/team2471/frc/steamworks/autonomouscommands/DriveBackToHopper.java
@@ -1,5 +1,6 @@
 package org.team2471.frc.steamworks.autonomouscommands;
 
+
 import org.team2471.frc.lib.motion_profiling.FollowPathTankDriveCommand;
 import org.team2471.frc.lib.motion_profiling.Path2D;
 import org.team2471.frc.steamworks.HardwareMap;
@@ -7,9 +8,12 @@ import org.team2471.frc.steamworks.HardwareMap;
 import static org.team2471.frc.steamworks.HardwareMap.DriveMap.shiftPTO;
 import static org.team2471.frc.steamworks.Robot.drive;
 
-public class DriveToHopperFromLeftLift extends FollowPathTankDriveCommand {
+//This auto only works for the blue alliance
+public class DriveBackToHopper extends FollowPathTankDriveCommand {
   Path2D m_path;
-  public DriveToHopperFromLeftLift(double speed, boolean mirror){
+
+  public DriveBackToHopper(double speed, boolean mirror) {
+
     requires(drive);
 
     setSpeed(speed);
@@ -18,15 +22,16 @@ public class DriveToHopperFromLeftLift extends FollowPathTankDriveCommand {
     setRightController(HardwareMap.DriveMap.rightMotor1);
 
     m_path = new Path2D();
-    m_path.setTravelDirection(1.0);
+    m_path.setTravelDirection(-1.0);
 
-    m_path.addPointAndTangent(4.8, 3.0, -3.0, 2.0);
-    m_path.addPointAndTangent(-1.0, 0.3, -4.0, -0.0);
+    m_path.addPointAndTangent(0.0, 0.0, -12.0, -5.0);
+    m_path.addPointAndTangent(-7.9, -1.8, -10.0, 0.0);
 
     m_path.addEasePoint(0.0, 0.0);
-    m_path.addEasePoint(3.0, 1.0);
+    m_path.addEasePoint(2.0, 1.0);
 
-    setPath(m_path);
+setPath(m_path);
+
   }
   @Override
   protected void initialize() {
@@ -34,3 +39,4 @@ public class DriveToHopperFromLeftLift extends FollowPathTankDriveCommand {
     shiftPTO.set(true);
   }
 }
+
