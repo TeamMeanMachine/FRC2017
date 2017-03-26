c5e5256e0f1c83d9fd828120c9ec49fd43a1d36c
tylerscheuble <tylerscheuble@gmail.com>
2017-03-25 20:41:05 -0700

    Spin up shooter early in forty KPA autos

---
+++ robot/src/main/java/org/team2471/frc/steamworks/commands/SpinUpShooterCommand.java
@@ -0,0 +1,23 @@
+package org.team2471.frc.steamworks.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.team2471.frc.steamworks.Robot;
+
+public class SpinUpShooterCommand extends Command {
+  private final double setpoint;
+
+  public SpinUpShooterCommand(double setpoint) {
+    this.setpoint = setpoint;
+    requires(Robot.shooter);
+  }
+
+  @Override
+  protected void execute() {
+    Robot.shooter.setSetpoint(setpoint);
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return isTimedOut();
+  }
+}
