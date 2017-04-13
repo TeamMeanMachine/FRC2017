8ea890bc62195aaf2e4a4e70d1aa15164321da5e
James-Kling <klingjt@gmail.com>
2017-04-12 20:59:35 -0700

    FeederLiftPlusTen auto

--- robot/src/main/java/org/team2471/frc/steamworks/autonomousroutines/CenterLiftPlusTen.java
+++ robot/src/main/java/org/team2471/frc/steamworks/autonomousroutines/CenterLiftPlusTen.java
@@ -2,8 +2,11 @@ package org.team2471.frc.steamworks.autonomousroutines;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.team2471.frc.steamworks.autonomouscommands.DriveDistanceCommand;
+import org.team2471.frc.steamworks.commands.AimCommand;
 import org.team2471.frc.steamworks.commands.PlaceGearCommand;
+import org.team2471.frc.steamworks.commands.TurnInPlaceCommand;
 //import org.team2471.frc.steamworks.commands.TiltGearIntakeCommand;
 
 public class CenterLiftPlusTen extends CommandGroup {
@@ -16,7 +19,8 @@ public class CenterLiftPlusTen extends CommandGroup {
     addSequential(new DriveDistanceCommand(6.5, 2.2));
     addParallel(new PlaceGearCommand(), 2);
     addSequential(new DriveDistanceCommand(-2.0, 1));
-//    addSequential(new TurnInPlaceCommand(-100.0, mirrored));
-//    addSequential(new AimCommand());
+    addSequential(new TurnInPlaceCommand(-100.0, mirrored));
+    addSequential(new DriveDistanceCommand(8.0, 2));
+    addSequential(new AimCommand(SmartDashboard.getNumber("RPM1",2471), (mirrored ? -1 : 1 )*150, 0.5));
   }
 }
